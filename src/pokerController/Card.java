2287e9dfd8083fee7090fa30ce69fb33da6b39df
Jules Daval <julesdaval@MacBook-Air-de-Jules.local>
2024-12-06 17:06:45 +0100

    Initial commit

---
+++ src/pokerController/Card.java
@@ -0,0 +1,17 @@
+package pokerController;
+
+public class Card implements Comparable<Card>{
+    private String color;
+    private String value;
+
+    public Card(String color,String value){
+        this.color = color;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        
+    }
+    
+}
---
+++ src/pokerController/Player.java
@@ -0,0 +1,10 @@
+package pokerController;
+
+import java.util.List;
+
+public class Player {
+    private String name;
+    private List<Card> hand;
+    private int stack;
+    
+}
