e4b48b286c6b8bca5e68dad95f785e8674e08688
Jules Daval <julesdaval@MacBook-Air-de-Jules.local>
2024-12-10 10:42:24 +0100

    implementation of Comparable<Combination> finished

--- src/pokerController/Combination.java
+++ src/pokerController/Combination.java
@@ -3,7 +3,7 @@ package pokerController;
 import java.util.Collections;
 import java.util.List;
 
-public class Combination {
+public class Combination implements Comparable<Combination>{
     private List<Card> combinationPlayer;
     
     public Combination(List<Card> combinationPlayer){
@@ -12,19 +12,30 @@ public class Combination {
     }
 
     public String[] pair(){
-        String[] res = new String[2];
+        String[] res = new String[4];
         res[0] = "N";
         String[] quad = this.quad();
         String[] set = this.set();
+        int count = 0;
         for(int i = 0; i < this.combinationPlayer.size() - 1; i++){
             if(this.combinationPlayer.get(i).getValue() == this.combinationPlayer.get(i + 1).getValue()){
                 if((quad[0] == "N" || quad[1] != this.combinationPlayer.get(i).getValue()) && (set[0] == "N" || set[1] != this.combinationPlayer.get(i).getValue())){
-                    res[0] = "Y";
-                    res[1] = this.combinationPlayer.get(i).getValue();
+                    count += 1;
+                    if(count == 1){
+                        res[0] = "Y";
+                        res[1] = "1";
+                        res[2] = this.combinationPlayer.get(i).getValue();
+                    }
+                    else{
+                        res[1] = "2";
+                        res[3] = this.combinationPlayer.get(i).getValue();
+                    }
+                    
                 }
                 
             }
         }
+        
         return res;
     }
 
@@ -56,10 +67,12 @@ public class Combination {
     }
 
     public String[] flush(){
-        String[] res = new String[2];
+        String[] res = new String[3];
         res[0] = "Y";
         String color = this.combinationPlayer.get(0).getColor();
         res[1] = color;
+        String value = this.combinationPlayer.getLast().getValue();
+        res[2] = value;
         for (Card card : combinationPlayer) {
             if(card.getColor() != color){
                 res[0] = "N";
@@ -104,6 +117,106 @@ public class Combination {
             res[2] = flush[1];
         }
         return res;
+    }
+
+
+    @Override
+    public int hashCode(){
+        String[] straightFlush = this.straightFlush();
+        String[] quads = this.quad();
+        String[] fullHouse = this.fullHouse();
+        String[] flush = this.flush();
+        String[] straight = this.straight();
+        String[] set = this.set();
+        String[] pair = this.pair();
+
+        int res = 0;
+
+        if(straightFlush[0] == "Y"){
+            Card card = new Card(straightFlush[2], straightFlush[1]);
+            int factor = card.hashCode();
+            res += factor * (Math.pow(15.0, 15.0));
+        }
+
+        if(quads[0] == "Y"){
+            Card card = new Card("S",quads[1]);
+            int factor = card.hashCode();
+            res += factor * (Math.pow(15.0,14.0));
+        }
+
+        if(fullHouse[0] == "Y"){
+            Card cardSet = new Card("S", fullHouse[1]);
+            Card cardPair = new Card("S", fullHouse[2]);
+            int factorSet = cardSet.hashCode();
+            int factorPair = cardPair.hashCode();
+            res += (factorSet * (Math.pow(15.0, 13.0))) + (factorPair * (Math.pow(15.0, 12.0))); 
+        }
+
+        if(flush[0] == "Y"){
+            Card card = new Card(flush[1], flush[2]);
+            int factor = card.hashCode();
+            res += factor * Math.pow(15.0, 11.0);
+        }
+
+        if(straight[0] == "Y"){
+            Card card = new Card("S", straight[1]);
+            int factor = card.hashCode();
+            res += factor * Math.pow(15.0, 10.0);
+        }
+
+        if(set[0] == "Y"){
+            Card card = new Card("S", set[1]);
+            int factor = card.hashCode();
+            res += factor * Math.pow(15.0, 9.0);
+        }
+
+        if(pair[0] == "Y" && Integer.parseInt(pair[1]) == 2){
+            Card lowestCard = new Card("S", pair[2]);
+            Card strongerCard = new Card("S", pair[3]);
+            int strongFactor = strongerCard.hashCode();
+            int lowFactor = lowestCard.hashCode();
+            res += (strongFactor * Math.pow(15.0, 8.0)) + (lowFactor * Math.pow(15.0, 7.0));
+        }
+
+        if(pair[0] == "Y" && Integer.parseInt(pair[1]) == 1){
+            Card card = new Card("S", pair[2]);
+            int factor = card.hashCode();
+            res += factor * Math.pow(15.0, 6.0);
+        }
+
+        double i = 5.0;
+        for (Card card : this.combinationPlayer) {
+            int factor = card.hashCode();
+            res += factor * Math.pow(15.0, i);
+            i -= 1.0;
+        }
+
+        return res;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(o == this){
+            return true;
+        }
+        if(!(o instanceof Combination)){
+            return false;
+        }
+        Combination combination = (Combination) o;
+        return combination.hashCode() == this.hashCode();
+    }
+
+    @Override
+    public int compareTo(Combination otherCombination){
+        if(this.equals(otherCombination)){
+            return 0;
+        }
+        if(this.hashCode() < otherCombination.hashCode()){
+            return -1;
+        }
+        else{
+            return 1;
+        }
     }
 
 
